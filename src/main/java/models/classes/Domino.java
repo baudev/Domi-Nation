5a4b3e4f50d110a8a42abe6190a2103a3c3e82f6
Baudev <baudev.contact@gmail.com>
2018-12-05 00:11:17 +0100

    Add LandPortion and number attributes to Domino class

--- src/main/java/models/classes/Domino.java
+++ src/main/java/models/classes/Domino.java
@@ -1,4 +1,50 @@
 package models.classes;
 
 public class Domino {
+
+    private LandPortion leftPortion;
+    private LandPortion rightPortion;
+
+    private int number;
+
+    /**
+     * Constructor
+     * @param leftPortion
+     * @param rightPortion
+     * @param number
+     */
+    public Domino(LandPortion leftPortion, LandPortion rightPortion, int number) {
+        this.setLeftPortion(leftPortion);
+        this.setRightPortion(rightPortion);
+        this.setNumber(number);
+    }
+
+    /**
+     *
+     * GETTERS AND SETTERS
+     *
+     */
+    public LandPortion getLeftPortion() {
+        return leftPortion;
+    }
+
+    public void setLeftPortion(LandPortion leftPortion) {
+        this.leftPortion = leftPortion;
+    }
+
+    public LandPortion getRightPortion() {
+        return rightPortion;
+    }
+
+    public void setRightPortion(LandPortion rightPortion) {
+        this.rightPortion = rightPortion;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
