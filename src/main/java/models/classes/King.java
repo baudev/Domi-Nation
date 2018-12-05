6c01f7254858134b3a860695faeece394ca9409c
Baudev <baudev.contact@gmail.com>
2018-12-05 16:05:28 +0100

    Create king class #8

---
+++ src/main/java/models/classes/King.java
@@ -0,0 +1,25 @@
+package models.classes;
+
+import models.enums.PlayerColor;
+
+public class King {
+
+    private PlayerColor color;
+
+    public King(PlayerColor color) {
+        this.color = color;
+    }
+
+    /**
+     *
+     * GETTERS AND SETTERS
+     *
+     */
+    public PlayerColor getColor() {
+        return color;
+    }
+
+    public void setColor(PlayerColor color) {
+        this.color = color;
+    }
+}
