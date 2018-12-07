1931b769573a0341731e54b3bc3008a6c0c76647
arthurbrochet <arthur.brochet@isep.fr>
2018-12-07 11:28:59 +0100

    getter and setter class Game

--- src/main/java/models/classes/Game.java
+++ src/main/java/models/classes/Game.java
@@ -7,4 +7,26 @@ public class Game {
     private List<Domino> dominoes;
     private List<Player> players;
 
+
+    /**
+     *
+     * GETTERS AND SETTERS
+     *
+     */
+
+    public List<Domino> getDominoes() {
+        return dominoes;
+    }
+
+    public void setDominoes(List<Domino> dominoes) {
+        this.dominoes = dominoes;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
 }
