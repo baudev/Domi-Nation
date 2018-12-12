ee8135549bb19dc9166ebce88922066ec42c3140
Baudev <baudev.contact@gmail.com>
2018-12-12 11:26:19 +0100

    Add associated string to GameMode enum

--- src/main/java/models/enums/GameMode.java
+++ src/main/java/models/enums/GameMode.java
@@ -2,10 +2,20 @@ package models.enums;
 
 public enum GameMode {
 
-    CLASSIC,
-    DYNASTY,
-    MIDDLEKINGDOM,
-    HARMOMY,
-    THEGREADDUEL;
+    CLASSIC("Classic"),
+    DYNASTY("Dynasty"),
+    MIDDLEKINGDOM("Middle Kingdom"),
+    HARMOMY("Harmony"),
+    THEGREATDUEL("The Great Duel");
 
+    private final String title;
+
+    GameMode(final String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
 }
