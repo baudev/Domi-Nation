2f00cf24d8295e9d8747866dac4c792d1a3755c9
Baudev <baudev.contact@gmail.com>
2018-12-14 01:05:37 +0100

    Add missing Board class

---
+++ src/main/java/models/classes/Board.java
@@ -0,0 +1,50 @@
+package models.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    private List<Domino> dominoes;
+    private Castle castle;
+    private StartTile startTile;
+
+    public Board() {
+        this.dominoes = new ArrayList<>();
+    }
+
+    /**
+     *
+     * GETTERS AND SETTERS
+     *
+     */
+
+    public List<Domino> getDominoes() {
+        return dominoes;
+    }
+
+    public void setDominoes(List<Domino> dominoes) {
+        this.dominoes = dominoes;
+    }
+
+    public void addDomino(Domino domino) {
+        this.dominoes.add(domino);
+    }
+
+    public Castle getCastle() {
+        return castle;
+    }
+
+    public void setCastle(Castle castle) {
+        this.castle = castle;
+    }
+
+    public StartTile getStartTile() {
+        return startTile;
+    }
+
+    public void setStartTile(StartTile startTile) {
+        this.startTile = startTile;
+    }
+
+}
