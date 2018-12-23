b2c332688f2dc82a83dd477637651d2ab871efd8
Baudev <baudev.contact@gmail.com>
2018-12-23 23:14:38 +0100

    Create DominoesList which extends from ArrayList and add a sort method

---
+++ src/main/java/models/classes/DominoesList.java
@@ -0,0 +1,24 @@
+package models.classes;
+
+import java.util.*;
+
+public class DominoesList extends ArrayList<Domino> {
+
+    /**
+     * Sort all Dominoes by their numbers
+     * @return
+     */
+    public List<Domino> sortByNumber() {
+        this.sort(new Comparator<Domino>() {
+            @Override
+            public int compare(Domino o1, Domino o2) {
+                if (o1.getNumber() == o2.getNumber()) {
+                    return 0;
+                }
+                return o1.getNumber() < o2.getNumber() ? -1 : 1;
+            }
+        });
+        return this;
+    }
+
+}
