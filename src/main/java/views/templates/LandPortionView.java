4a16d51d62c7d8b41b28b30b4f744565b5b4b31a
Baudev <baudev.contact@gmail.com>
2018-12-25 23:18:32 +0100

    Create LandPortionView class which draw LandType and the corresonding number of crowns

---
+++ src/main/java/views/templates/LandPortionView.java
@@ -0,0 +1,47 @@
+package views.templates;
+
+import helpers.Screen;
+import javafx.scene.Parent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import models.classes.LandPortion;
+
+
+public class LandPortionView extends Parent {
+
+    private LandPortion landPortion;
+
+    public LandPortionView(LandPortion landPortion) {
+        this.landPortion = landPortion;
+        addPortionType();
+        addCrowns();
+    }
+
+    private void addPortionType() {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setFill(landPortion.getLandPortionType().getValue()); // set color
+        rectangle.setWidth(Screen.percentageToXDimension(4));
+        rectangle.setHeight(Screen.percentageToYDimension(5));
+        this.getChildren().add(rectangle);
+    }
+
+    private void addCrowns() {
+        for(int i = 0; i < landPortion.getNumberCrowns(); i++) {
+            Circle circle = new Circle();
+            circle.setFill(Color.GREENYELLOW);
+            circle.setRadius(Screen.percentageToXDimension(0.5));
+            circle.setCenterX(Screen.percentageToXDimension(1.1*i + 0.6));
+            circle.setCenterY(Screen.percentageToYDimension(1));
+            this.getChildren().add(circle);
+        }
+    }
+
+    public LandPortion getLandPortion() {
+        return landPortion;
+    }
+
+    public void setLandPortion(LandPortion landPortion) {
+        this.landPortion = landPortion;
+    }
+}
