eb86ddb2fc025720b601ff2ccb23bab88582b09c
Baudev <baudev.contact@gmail.com>
2018-12-26 00:58:32 +0100

    Make extends FunctionTest from Function for coverage

--- src/test/java/helpers/FunctionTest.java
+++ src/test/java/helpers/FunctionTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FunctionTest {
+class FunctionTest extends Function {
 
     @Test
     void randInt() {
