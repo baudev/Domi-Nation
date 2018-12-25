003540a3d98eec5a653ba2f4fbee1c90adcc2981
Baudev <baudev.contact@gmail.com>
2018-12-25 22:47:38 +0100

    Add corresponding javafx to LandPortionType

--- src/main/java/models/enums/LandPortionType.java
+++ src/main/java/models/enums/LandPortionType.java
@@ -1,13 +1,26 @@
 package models.enums;
 
+import javafx.scene.paint.Color;
+
 public enum LandPortionType {
 
-    CHAMPS,
-    FORET,
-    MER,
-    PRAIRIE,
-    MINE,
-    MONTAGNE,
-    TOUS;
+    CHAMPS(Color.YELLOW),
+    FORET(Color.GREEN),
+    MER(Color.BLUE),
+    PRAIRIE(Color.RED),
+    MINE(Color.GREY),
+    MONTAGNE(Color.WHITESMOKE),
+    TOUS(Color.PINK);
+
+    private Color color;
+
+    LandPortionType(Color color) {
+        this.color = color;
+    }
+
+    public Color getValue() {
+        return this.color;
+    }
+
 
 }
