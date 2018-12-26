a3ef092e292270dbc9c41b96d8218e68ba53dde6
Baudev <baudev.contact@gmail.com>
2018-12-26 14:03:07 +0100

    Add javafx color equivalent for PlayerColor

--- src/main/java/models/enums/PlayerColor.java
+++ src/main/java/models/enums/PlayerColor.java
@@ -1,13 +1,25 @@
 package models.enums;
 
+import javafx.scene.paint.Color;
+
 /**
  * Different possible colors for a player
  */
 public enum PlayerColor {
 
-    PINK,
-    YELLOW,
-    GREEN,
-    BLUE
+    PINK(Color.PINK),
+    YELLOW(Color.YELLOW),
+    GREEN(Color.GREEN),
+    BLUE(Color.BLUE);
+
+    private Color color;
+
+    PlayerColor(Color color) {
+        this.color = color;
+    }
+
+    public Color getValue() {
+        return this.color;
+    }
 
 }
