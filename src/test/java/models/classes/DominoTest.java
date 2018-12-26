29a669edc0e819330939906a7c62f2943f4eee43
Baudev <baudev.contact@gmail.com>
2018-12-26 12:49:34 +0100

    Add methods for testing DominoView setter and getter in Domino class

--- src/test/java/models/classes/DominoTest.java
+++ src/test/java/models/classes/DominoTest.java
@@ -2,9 +2,14 @@ package models.classes;
 
 import exceptions.MaxCrownsLandPortionExceeded;
 import helpers.Config;
+import mockit.Mock;
+import mockit.MockUp;
 import models.enums.LandPortionType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import views.templates.DominoView;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +23,13 @@ class DominoTest {
         landPortionLeft = new LandPortion(Integer.valueOf(Config.getValue("LandPortionMaxCrowns")), LandPortionType.CHAMPS);
         landPortionRight = new LandPortion(Integer.valueOf(Config.getValue("LandPortionMaxCrowns")) - 1, LandPortionType.MINE);
         domino = new Domino(landPortionLeft, landPortionRight, 50);
+
+        // Mock the DominoView class
+        new MockUp<DominoView>() {
+            @Mock
+            public void $init(Domino domino) {
+            }
+        };
     }
 
     @Test
@@ -34,4 +46,18 @@ class DominoTest {
     void getNumber() {
         assertEquals(50, domino.getNumber());
     }
+
+    @Test
+    void setDominoViewShouldReturnSame() {
+        DominoView dominoView = new DominoView(domino);
+        domino.setDominoView(dominoView);
+        assertSame(dominoView, domino.getDominoView());
+    }
+
+    @Test
+    void setNullDominoViewShouldReturnDominoView() {
+        domino.setDominoView(null);
+        assertTrue(domino.getDominoView() instanceof DominoView);
+    }
+
 }
