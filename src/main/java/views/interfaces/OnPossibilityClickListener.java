1b3f1e49c333414bd439c523a636e97acb70a1bf
Baudev <baudev.contact@gmail.com>
2019-01-05 19:13:15 +0100

    Set method in OnPossibilityClickListener interface

--- src/main/java/views/interfaces/OnPossibilityClickListener.java
+++ src/main/java/views/interfaces/OnPossibilityClickListener.java
@@ -1,4 +1,9 @@
 package views.interfaces;
 
+import models.classes.Position;
+
 public interface OnPossibilityClickListener {
+
+    void onPossibilityClickListener(Position position1, Position position2);
+
 }
