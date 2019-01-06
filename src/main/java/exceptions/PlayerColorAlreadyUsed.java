242a5cfb1ef341c6b5485ae84c811a592b8b99e3
Baudev <baudev.contact@gmail.com>
2019-01-06 23:55:33 +0100

    Comment exception classes

--- src/main/java/exceptions/PlayerColorAlreadyUsed.java
+++ src/main/java/exceptions/PlayerColorAlreadyUsed.java
@@ -1,4 +1,10 @@
 package exceptions;
 
+import models.enums.PlayerColor;
+
+/**
+ * A {@link models.classes.Player} is already using the {@link models.enums.PlayerColor} desired.
+ * @see models.classes.Game#createPlayerWithColor(PlayerColor)
+ */
 public class PlayerColorAlreadyUsed extends Exception {
 }
