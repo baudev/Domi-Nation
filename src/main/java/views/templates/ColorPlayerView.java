c152bd8c0deabfa5c5ffc303c2bc2216aea3e632
arthurbrochet <arthur.brochet@isep.fr>
2019-01-10 11:27:11 +0100

    Game mode View + NumberView + ColorPlayerView interface
    - need to change the button

--- src/main/java/views/templates/ColorPlayerView.java
+++ src/main/java/views/templates/ColorPlayerView.java
@@ -6,9 +6,15 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import models.enums.PlayerColor;
 import views.interfaces.OnPlayerColorClickListener;
 
@@ -19,14 +25,42 @@ public class ColorPlayerView extends Parent {
     private OnPlayerColorClickListener onPlayerColorClickListener;
 
     public ColorPlayerView(List<PlayerColor> playerColorList, int playerNumber) {
-        // Define layout
-        GridPane gridPane = new GridPane();
+
+        // Define background
+
+        Image background = new Image("/kingDomino.png");        //add the background image of the game
+        ImageView imageView = new ImageView(background);
+        imageView.setFitWidth(Screen.percentageToXDimension(100));
+        imageView.setFitHeight(Screen.percentageToYDimension(100));
+        this.getChildren().add(imageView);
+
+        // Define banner
+        StackPane stackPane1 = new StackPane();                 //add the banner image
+        Image imageParchemin = new Image("/parchemin.png");
+        ImageView parchemin = new ImageView(imageParchemin);
+        stackPane1.getChildren().add(parchemin);
+        stackPane1.setMinSize(Screen.percentageToXDimension(100), Screen.percentageToYDimension(100));  //positioning the banner image
+        stackPane1.setAlignment(Pos.CENTER);
+        stackPane1.setTranslateY(Screen.percentageToYDimension(-20));
+        this.getChildren().add(stackPane1);
+
+
 
         // Define title
+        StackPane stackPane = new StackPane();          //add the title on the banner
         Text text = new Text();
         text.setText("Player " + playerNumber + ", select a color");
-        gridPane.add(text, 1, 0);
+        text.setFont(Font.font("null", FontWeight.BOLD,35));
+        text.setTextAlignment(TextAlignment.CENTER);
+        stackPane.getChildren().add(text);
+        stackPane.setAlignment(Pos.CENTER);
+        stackPane.setMinSize(Screen.percentageToXDimension(100), Screen.percentageToYDimension(50)); // define the size of the stackpane layout
 
+        this.getChildren().add(stackPane);
+
+
+        HBox hBox = new HBox(); // create an Hbox to align horizontally the button
+        hBox.setSpacing(20);    // set the space between each button
         // Add the buttons of each possibilities
         int i = 0;
         for(models.enums.PlayerColor playerColor : playerColorList){
@@ -42,13 +76,14 @@ public class ColorPlayerView extends Parent {
                 }
             });
 
-            gridPane.add(button, i, 1); // add the button to the layout
+            hBox.getChildren().add(button); // add the button to the layout
             i++;
         }
-
-        gridPane.setMinSize(Screen.percentageToXDimension(100), Screen.percentageToYDimension(100)); // define the size of the gridPane layout
-        gridPane.setAlignment(Pos.CENTER); // center the layout
-        this.getChildren().add(gridPane); // add the layout to the root group
+        hBox.setMinSize(Screen.percentageToXDimension(80), Screen.percentageToYDimension(60)); // define the size of the hbox layout
+        hBox.setTranslateX(Screen.percentageToXDimension(10));
+        hBox.setTranslateY(Screen.percentageToYDimension(30));
+        hBox.setAlignment(Pos.CENTER);  //center the layout
+        this.getChildren().add(hBox); // add the layout to the root group
     }
 
     /**
